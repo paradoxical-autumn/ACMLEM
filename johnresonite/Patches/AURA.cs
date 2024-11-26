c0a254e1ec918fd40e0ab1512fa4d3e33b18284e
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-11-26 19:34:48 +0000

    init

---
+++ johnresonite/Patches/AURA.cs
@@ -0,0 +1,39 @@
+﻿using FrooxEngine;
+using FrooxEngine.CommonAvatar;
+using FrooxEngine.ProtoFlux;
+using HarmonyLib;
+using MonkeyLoader.Patching;
+using MonkeyLoader.Resonite;
+using System.Collections.Generic;
+
+namespace JohnResoniteMod
+{
+    [HarmonyPatchCategory(nameof(AURA_Patch))]
+    [HarmonyPatch(typeof(AvatarNameTagAssigner), "SetLabel")]
+    internal class AURA_Patch : ConfiguredResoniteMonkey<AURA_Patch, JohnResoniteConfig>
+    {
+        public override bool CanBeDisabled => true;
+
+        private static void Postfix(AvatarUserReferenceAssigner __instance, SyncRefList<IField<string>> ___LabelTargets)
+        {
+            if (!Enabled)
+            {
+                return;
+            }
+
+            if (___LabelTargets == null)
+            {
+                //Logger.Error(() => "Label targets == null!!");
+                return;
+            }
+
+            foreach (IField<string> target in ___LabelTargets)
+            {
+                if (target != null)
+                {
+                    target.Value = ConfigSection.UserName;
+                }
+            }
+        }
+    }
+}
---
+++ johnresonite/Settings/JohnResoniteConfig.cs
@@ -0,0 +1,22 @@
+﻿using MonkeyLoader.Configuration;
+
+namespace JohnResoniteMod;
+
+internal sealed class JohnResoniteConfig : ConfigSection
+{
+    // define variables
+    private readonly DefiningConfigKey<string> _userName = new("Name", "What should people be called? The only correct answer is \"john resonite\".", () => "john resonite");
+
+    // set internal variables
+    public override string Description => "Options";
+    public override string Id => "JohnResonite.MainSettings";
+    public override string Name => "john's settings.";
+    public override Version Version { get; } = new Version(1, 0, 0);
+
+    // init variables
+    public string UserName
+    {
+        get => _userName.GetValue()!;
+        set => _userName.SetValue(value);
+    }
+}
